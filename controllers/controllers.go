728128e32e461fdcc73ff7b6500d8125a0e67ec2
Meghamsh <meghamalyala02@gmail.com>
2023-10-24 23:24:20 +0530

    Controllers seperated

---
+++ controllers/controllers.go
@@ -0,0 +1,236 @@
+package controllers
+
+import (
+	"fmt"
+	"math"
+	"net/http"
+	"sendx/fetch_content"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type Job struct { // Convert the URL to a job
+	URL            string
+	ResultChannel  chan string // Storing the result in this channel
+	isPaidCustomer bool
+	retries        int
+}
+
+var (
+	MaxPaidWorkers    = 5 // Workers for crawling for paid Users
+	MaxNonPaidWorkers = 2 // Workers for crawling for un-paid Users
+
+	maxRequestsPerHour = math.MaxInt64 // Setting max requests initially as Maximum value
+
+	reqMutex sync.Mutex     // Mutex to control the incoming requests if ourworkers are shutdown
+	wg       sync.WaitGroup // To check if all our workers are shutdown
+
+	// Seperate JobQueue for paid and nonpaid workers
+	paidJobQueue    chan Job
+	nonPaidJobQueue chan Job
+
+	shutdown chan struct{} // To give signal for workers to stop
+)
+
+// Requests of this window size is stored
+const windowSize = time.Hour
+
+// func
+
+func Main() {
+	fetch_content.Main() // Starting main function of Fetch Content
+
+	// Used for shutting down workers
+	shutdown = make(chan struct{}, MaxPaidWorkers+MaxPaidWorkers)
+
+	paidJobQueue = make(chan Job, MaxPaidWorkers)
+	nonPaidJobQueue = make(chan Job, MaxPaidWorkers)
+
+	// Starting Workers
+	for i := 1; i <= MaxPaidWorkers; i++ {
+		wg.Add(1)
+		go Worker(i, true)
+	}
+
+	for i := 1; i <= MaxNonPaidWorkers; i++ {
+		wg.Add(1)
+		go Worker(i, false)
+	}
+}
+
+// To set the crawling speed
+func SetSpeed(w http.ResponseWriter, r *http.Request) {
+	new_noOfReqStr := r.URL.Query().Get("speed")
+	new_noOfReq, err := strconv.Atoi(new_noOfReqStr)
+	if err != nil || new_noOfReq < 0 {
+		fmt.Fprintf(w, "Invalid crawling speed")
+		return
+	}
+	maxRequestsPerHour = new_noOfReq
+	fmt.Fprintf(w, "Crawling speed set to %d", new_noOfReq)
+}
+
+// To handle url crawl requests
+// Creating job and pushing into corresponding job queue
+func CrawlHandler(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Query().Get("url")
+	isPayingCustomer := false
+	isPayingCustomer = r.URL.Query().Get("isPaidCustomer") == "1"
+
+	tries := 2
+	if isPayingCustomer {
+		tries++
+	}
+	job := Job{
+		URL:            url,
+		ResultChannel:  make(chan string),
+		isPaidCustomer: isPayingCustomer,
+		retries:        tries,
+	}
+	reqMutex.Lock()
+
+	//Pushing into job queue
+	if isPayingCustomer {
+		paidJobQueue <- job
+	} else {
+		nonPaidJobQueue <- job
+	}
+
+	reqMutex.Unlock()
+	result := <-job.ResultChannel // To obtain result from the result Channel
+	job.retries--
+
+	// Pushing the job into jobqueue again if fetching is failed, until a certain limit of retries
+	if result == "" && job.retries > 0 {
+		fmt.Println("Haaa")
+		if isPayingCustomer {
+			paidJobQueue <- job
+		} else {
+			nonPaidJobQueue <- job
+		}
+		result = <-job.ResultChannel
+	}
+	if result == "" {
+		fmt.Fprintf(w, "Unable to fetch the given URL after %d tries", tries)
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprint(w, result)
+}
+
+var workersChange sync.Mutex // Mutex to handle change in number of workers
+
+func SetCrawlers(w http.ResponseWriter, r *http.Request) {
+	new_paidWorkers := MaxPaidWorkers
+	new_unpaidWorkers := MaxNonPaidWorkers
+	var err1 error = nil
+	var err2 error = nil
+	new_paidWorkers, err1 = strconv.Atoi(r.URL.Query().Get("paidWorkers"))
+	new_unpaidWorkers, err2 = strconv.Atoi(r.URL.Query().Get("unpaidWorkers"))
+
+	if err1 != nil && err2 != nil {
+		// Error handling
+		fmt.Println(err1, err2)
+		fmt.Fprintf(w, "Input error, Please set the value correctly")
+		return
+	}
+
+	workersChange.Lock()
+	defer workersChange.Unlock()
+	initWorkers(new_paidWorkers, new_unpaidWorkers)
+	if err1 != nil {
+		fmt.Fprintf(w, "Set Number of workers for Non paying Users to %d", new_unpaidWorkers)
+	} else if err2 != nil {
+		fmt.Fprintf(w, "Set Number of workers for Paying Users to %d", new_paidWorkers)
+	} else {
+		fmt.Fprintf(w, "Set Number of workers for Paying Users to %d and Non paying Users to %d", new_paidWorkers, new_unpaidWorkers)
+	}
+}
+
+func Worker(id int, forPaid bool) {
+	var requestTimes []time.Time // To maintain the count of requests in past hour
+	var JobQueue chan Job
+
+	// Setting Queue based on worker type
+	if forPaid {
+		JobQueue = paidJobQueue
+	} else {
+		JobQueue = nonPaidJobQueue
+	}
+	defer wg.Done()
+	for {
+		select {
+		case <-shutdown:
+			return //Signal for shutting down
+		case job := <-JobQueue:
+			fmt.Printf("Job done by worker with job id : %d\n", id)
+			currentTime := time.Now()
+
+			//Removing requests which are older than 1 hour
+			var i int
+			for i = 0; i < len(requestTimes); i++ {
+				if currentTime.Sub(requestTimes[i]) <= windowSize {
+					break
+				}
+			}
+			if i > 0 {
+				requestTimes = requestTimes[i:]
+			}
+			// for len(requestTimes) > 0 && currentTime.Sub(requestTimes[0]) > windowSize {
+			// 	requestTimes = requestTimes[1:]
+			// }
+
+			if len(requestTimes) >= maxRequestsPerHour {
+				job.ResultChannel <- "Rate limit exceeded. Try again later."
+				continue
+			}
+			requestTimes = append(requestTimes, currentTime) // Adding current request
+			result, _ := fetch_content.FetchContent(job.URL, job.isPaidCustomer)
+			job.ResultChannel <- result
+		}
+	}
+}
+
+// Re initialise workers to update the workers count
+func initWorkers(new_paidWorkers, new_unpaidWorkers int) {
+
+	// Signals for shutting down the workers
+	for i := 1; i <= MaxNonPaidWorkers+MaxPaidWorkers; i++ {
+		shutdown <- struct{}{}
+	}
+	wg.Wait()
+
+	MaxPaidWorkers = new_paidWorkers
+	MaxNonPaidWorkers = new_unpaidWorkers
+	newPaidJobQueue := make(chan Job, MaxPaidWorkers) // Update the queue size accordingly
+	newNonPaidJobQueue := make(chan Job, MaxNonPaidWorkers)
+
+	reqMutex.Lock() // Stop incoming requests to jobqueue to avoid race condition and make sure that tasks are not lost
+	defer reqMutex.Unlock()
+
+	// Shift all the jobs from old jobqueue to the new jobqueue
+	for i := 0; i < len(paidJobQueue); {
+		newPaidJobQueue <- <-paidJobQueue
+	}
+	for i := 0; i < len(nonPaidJobQueue); {
+		newNonPaidJobQueue <- <-nonPaidJobQueue
+	}
+	paidJobQueue = newPaidJobQueue
+	nonPaidJobQueue = newNonPaidJobQueue
+	// After all the workers are down new workers are started
+	for i := 1; i <= new_paidWorkers; i++ {
+		wg.Add(1)
+		go Worker(i, true)
+	}
+
+	for i := 1; i <= new_unpaidWorkers; i++ {
+		wg.Add(1)
+		go Worker(i, false)
+	}
+	close(shutdown)
+	shutdown = make(chan struct{}, MaxPaidWorkers+MaxPaidWorkers)
+	// fmt.Printf("Number of paid workers set to %d and number of non-paid workers set to %d \n", MaxPaidWorkers, MaxNonPaidWorkers)
+}
